098309f12b4c9c562117981258257b33b63b389e
Julie Cook <juliecook328@gmail.com>
2019-05-30 22:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  kittens.slice(0, kittens.length-1)
-  return kittens
+  var lessKittens = kittens.slice(0, kittens.length-1)
+  return lessKittens
 }
